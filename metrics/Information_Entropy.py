8cfcc61d10d386ef88ee021c17ec8b59263af4cf
Junchi Yao <145746449+YokeYao@users.noreply.github.com>
2025-06-11 17:51:53 +0800

    Update Information_Entropy.py

--- metrics/Information_Entropy.py
+++ metrics/Information_Entropy.py
@@ -10,23 +10,7 @@ def generate_ngrams(text, n):
     ngrams = zip(*[words[i:] for i in range(n)])
     return [" ".join(ngram) for ngram in ngrams]
 
-# 根据token数量归一化
-# def calculate_ngram_entropy(text, n):
-#     ngrams = generate_ngrams(text, n)
-#     ngram_counts = Counter(ngrams)
-#     total_ngrams = sum(ngram_counts.values())
-#
-#     probabilities = [count / total_ngrams for count in ngram_counts.values()]
-#     entropy = calculate_entropy(probabilities)
-#
-#     # 获取 token 的数量
-#     total_tokens = len(text.split())
-#
-#     # 归一化熵（将熵值除以 token 数）
-#     normalized_entropy = entropy / total_tokens * 10
-#     return normalized_entropy
 
-# 最大熵标准化
 def calculate_ngram_entropy(text, n):
     """Calculate the normalized entropy of n-grams for a given text."""
     ngrams = generate_ngrams(text, n)
@@ -45,7 +29,6 @@ def calculate_ngram_entropy(text, n):
 
 if __name__ == '__main__':
 
-    # 定义所有文本
     texts = {
         "normal 0": "Option c) Rs 550 is the correct answer. This choice is accurate because it aligns with the calculations or criteria provided in the problem statement. Selecting this option ensures the correct interpretation and understanding of the given information, confirming Rs 550 as the appropriate solution.",
         "single token repeat": "Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option Option ",
@@ -62,9 +45,8 @@ if __name__ == '__main__':
         "short term5": " is the of of the that you is are not to?",
     }
     test = "Once upon a time, Emily held and rocked Kayla as they both sobbed because Kayla really needed a good friend. You see, little Kayla was feeling very lonely and sad because she didn't have anyone to play with or talk to.\n\nEmily wanted to help Kayla and be her friend, so she did something truly special. Emily decided to throw a magical friendship party for Kayla. Emily knew that this would cheer her up and help her make some new friends!\n\nEmily worked really hard, preparing for the big day. She bought balloons, baked cookies, and even created a treasure hunt for everyone to enjoy. She invited lots of other kids from their neighborhood, so Kayla could meet them all and find a friend to play with.\n\nOn the day of the party, the sun was shining, and the sky was filled with fluffy clouds. All the kids in the neighborhood were excited to come to the party and play with Kayla.\n\nEmily and Kayla welcomed their guests with huge smiles and warm hugs. The kids played games, laughed, and explored the treasure hunt that Emily had set up all around the backyard.\n\nSoon, Kayla was surrounded by new friends, chatting and laughing together. She began to feel so happy and loved. As the sun began to set and the party ended, Kayla felt like she finally had found the friends she needed.\n\nThat night, as Emily tucked Kayla into bed, Kayla thanked her for being such an amazing friend and throwing the best party ever. Emily smiled and said, \"That's what friends are for, to help each other when we're feeling sad and lonely.\"\n\nAnd from that day on, Emily and Kayla were the best of friends and shared many more magical adventures together. Now, Kayla knew that she always had a good friend by her side. good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side.good friend by her side."
-    n = 1  # 计算双词组（二元组）的信息熵
+    n = 1  
 
-    # 对每个文本计算信息熵并打印结果
     for name, text in texts.items():
         entropy = calculate_ngram_entropy(text, n)
         print(f"The {n}-gram entropy of {name} is: {entropy:.4f}")
